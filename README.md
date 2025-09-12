925d4d6a03bbc58c7ec595cb553fe899d29c8149
glckfndr <obulan65@gmail.com>
2025-09-12 16:45:25 +0300

    feat: add Chart

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Project from Advanced React course on Scrimba
 https://scrimba.com/fullstack-path-c0fullstack/~05mn
 npm i @supabase/supabase-js
+npm i react-charts@beta --save
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,6 @@
 import supabase from "./supabase-client";
 import { useEffect, useState } from "react";
+import { Chart } from "react-charts";
 
 function Dashboard() {
   const [metrics, setMetrics] = useState([]);
@@ -23,10 +24,57 @@ function Dashboard() {
     }
   }
 
+  const chartData = [
+    {
+      data: metrics.map((m) => ({ primary: m.name, secondary: m.sum })),
+    },
+  ];
+  const primaryAxis = {
+    getValue: (d) => d.primary,
+    scaleType: "band",
+    padding: 0.2,
+    position: "bottom",
+  };
+
+  const secondaryAxes = [
+    {
+      getValue: (d) => d.secondary,
+      scaleType: "linear",
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ];
+
+  function y_max() {
+    if (metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum));
+      return maxSum + 2000;
+    }
+    return 5000;
+  }
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          <Chart
+            options={{
+              data: chartData,
+              primaryAxis,
+              secondaryAxes,
+              type: "bar",
+              defaultColors: ["#58d675"],
+              tooltip: {
+                show: false,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
