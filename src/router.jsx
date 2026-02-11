89cd1358c21414bcf9c68af4965b2da3d03dfcd6
glckfndr <obulan65@gmail.com>
2026-02-12 00:15:53 +0200

    feat: add ProtectedRoute

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import Signup from "./components/Signup";
 import Header from "./components/Header";
 import Dashboard from "./routes/Dashboard";
 import RootRedirect from "./routes/RootRedirect";
+import ProtectedRoute from "./routes/ProtectedRoute";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -21,10 +22,10 @@ export const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: (
-      <>
+      <ProtectedRoute>
         <Header />
         <Dashboard />
-      </>
+      </ProtectedRoute>
     ),
   },
 ]);
